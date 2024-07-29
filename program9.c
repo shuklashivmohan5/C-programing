2ab86be432c39b11c57e3fff86f6ae4aa2c035a6
shuklashivmohan5 <shuklashivmohan19@gmail.com>
2024-07-29 12:19:47 +0530

    done

---
+++ program9.c
@@ -0,0 +1,43 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i=1;
+
+//     do
+//     {
+//         printf("%d ",i);
+//         i++;
+//     }   while (i<=10);
+    
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i=10;
+//     do
+//     {
+//         printf("%d ",i);
+//         i--;
+//     } while (i>=1);
+
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i;
+//     for ( i=10; i>=1; i--)
+//     {
+//         printf("%d ",i);
+//     }
+    
+//     return 0;
+// }
